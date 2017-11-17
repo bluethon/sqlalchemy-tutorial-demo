d681668988eef18711483ceba50f547c7be15b39
bluethon <j5088794@gmail.com>
2017-11-17 17:04:37 +0800

    finish

--- test.py
+++ test.py
@@ -556,6 +556,7 @@ print('========================================')
 
 from sqlalchemy import Table, Text
 
+# 如果需要自定义主键, 或者和其他表的外键, 需要使用Association  Object(类定义), 不能使用如下
 # association table
 post_keywords = Table('post_keywords', Base.metadata,
                       Column('post_id', ForeignKey('posts.id'), primary_key=True),
@@ -569,3 +570,62 @@ class BlogPost(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     headline = Column(String(255), nullable=False)
     body = Column(Text)
+    
+    # many to many BlogPost<->Keyword
+    keywords = relationship('Keyword',
+                            secondary=post_keywords,
+                            back_populates='posts')
+    
+    def __init__(self, headline, body, author):
+        self.author = author
+        self.headline = headline
+        self.body = body
+    
+    def __repr__(self):
+        return f'BlogPost({self.headline!r}, {self.body!r}, {self.author!r})'
+        # return "BlogPost(%r, %r, %r)" % (self.headline, self.body, self.author)
+
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+    
+    id = Column(Integer, primary_key=True)
+    keyword = Column(String(50), nullable=False, unique=True)
+    posts = relationship('BlogPost',
+                         secondary=post_keywords,
+                         back_populates='keywords')
+    
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+
+BlogPost.author = relationship(User, back_populates='posts')
+User.posts = relationship(BlogPost, back_populates='author', lazy='dynamic')
+
+Base.metadata.create_all(engine)
+
+wendy = session.query(User). \
+    filter_by(name='wendy'). \
+    one()
+
+post = BlogPost("Wendy's Blog Post", 'This is a test', wendy)
+session.add(post)
+
+post.keywords.append(Keyword('wendy'))
+post.keywords.append(Keyword('firstpost'))
+
+print(session.query(BlogPost).
+      filter(BlogPost.keywords.any(keyword='firstpost')).
+      all()
+      )
+
+print(session.query(BlogPost).
+      filter(BlogPost.author == wendy).
+      filter(BlogPost.keywords.any(keyword='firstpost')).
+      all()
+      )
+
+print(wendy.posts.
+      filter(BlogPost.keywords.any(keyword='firstpost')).
+      all()
+      )
